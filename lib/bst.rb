5751176b9c2c388f6d0bfe9fb97477382e77b8df
otherdcn <danny.c.ntwari@gmail.com>
2024-08-31 23:11:04 +0200

    Add #delete and other helper methods to Tree

--- lib/bst.rb
+++ lib/bst.rb
@@ -48,6 +48,43 @@ class Tree
     return test_node
   end
 
+  def replace_with_successor(curr)
+    curr = curr.right_child
+    while !(curr.nil?) && !(curr.left_child.nil?)
+      curr = curr.left_child
+    end
+
+    curr
+  end
+
+  def delete(node, test_node = root_node)
+    if empty?
+      return nil
+    elsif test_node.nil?
+      return nil
+    elsif node < test_node.data
+      test_node.left_child = delete(node, test_node.left_child)
+    elsif node > test_node.data
+      test_node.right_child = delete(node, test_node.right_child)
+    elsif node == test_node.data
+      if test_node.left_child.nil? && test_node.right_child.nil?
+        return nil
+      elsif !(test_node.left_child.nil?) && !(test_node.right_child.nil?)
+        successor = replace_with_successor(test_node)
+        test_node.data = successor.data
+        test_node.right_child = delete(successor.data, test_node.right_child)
+      elsif test_node.left_child.nil?
+        return test_node.right_child
+      elsif test_node.right_child.nil?
+        return test_node.left_child
+      else
+        puts "Else clause"
+      end
+    end
+
+    test_node
+  end
+
   def pretty_print(node = root_node, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
